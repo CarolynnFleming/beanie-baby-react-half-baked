3d0fb4bfddbf01269e2beb0607c3197117c89968
CarolynnFleming <cfcatpink@gmail.com>
2022-02-07 17:44:43 -0500

    onclick fixed

--- src/BeaniesPage.js
+++ src/BeaniesPage.js
@@ -30,7 +30,7 @@ function App() {
           disabled={page === 1}>Previous Page</button>
         {/* on click, this button should increment the page in state  */}
         <button onClick={() => setPage(page + 1)}
-          disabled={page === 1}>Next Page</button>
+          disabled={beanieBabies.length < perPage}>Next Page</button>
       </div>
       {/* pass the beanie babies into the BeaniesList component */}
       <BeaniesList beanieBabies={beanieBabies}/>
