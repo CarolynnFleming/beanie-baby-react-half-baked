034e0c8d13f7da90be96b12a200f513ccec95021
CarolynnFleming <cfcatpink@gmail.com>
2022-02-07 15:58:27 -0500

    changed beanie baby

--- src/BeanieBaby.js
+++ src/BeanieBaby.js
@@ -4,16 +4,13 @@ import { Link } from 'react-router-dom';
 export default function BeanieBaby({ beanieBaby }) {
   return (
     // eslint-disable-next-line react/jsx-no-comment-textnodes
-    <section>
     // this should contain a react-router-dom Link to the detail page for this particular beanie baby.
     // eslint-disable-next-line react/no-unescaped-entities
     // it should also render the beanie baby's image and show the beanie baby's name
-      {beanieBaby.map((baby, i) => <Link key={`${baby.title}-${i}`} to={`/detail/${baby.id}`}>
-        <div>
-          {baby.title}
-          <img src={baby.image}/>
-        </div>
-      </Link>)}
-    </section>
+    <Link to={`/detail/${beanieBaby.id}`}>
+      <p>{beanieBaby.title}</p>
+      <img src={beanieBaby.image}/>
+    </Link> 
+  
   );
 }
